22d1a27806caf641003ce4a3317e5336a16caa37
D <deskisaptapratama@gmail.com>
2019-05-04 01:11:56 +0800

    fix some code

--- example_bot.py
+++ example_bot.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     for extension in startup_extensions:
         try:
 
-            Iori.load_extension(extension)
+            bot.load_extension(extension)
             print(f'{extension} Has Been Loaded!  All {extension} Command Is Up!')
         except Exception as e:
             exc = '{}: {}'.format(type(e).__name__, e)
@@ -34,16 +34,16 @@ if __name__ == "__main__":
 # creating Random Presence
 @bot.event
 async def presence():
-    await Iori.wait_until_ready()
-    while not Iori.is_closed():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
         get_info = random.randint(1, 3)
         if get_info == 1:
-            thing = ('In {} Servers'.format(len(Iori.guilds)))
+            thing = ('In {} Servers'.format(len(bot.guilds)))
         if get_info == 2:
-            thing = ('With {} Users'.format(len(list(Iori.get_all_members()))))
+            thing = ('With {} Users'.format(len(list(bot.get_all_members()))))
         if get_info == 3:
             thing = "Prefixes: 'e:'"
-        await Iori.change_presence(status=status.dnd, activity=discord.Game(name=thing))
+        await bot.change_presence(status=status.dnd, activity=discord.Game(name=thing))
         await asyncio.sleep(15)
 
 
@@ -51,10 +51,9 @@ async def presence():
 # startup bot
 @bot.event
 async def on_ready():
-    Iori.loop.create_task(bot.presence()) #add this line for random presence
+    bot.loop.create_task(bot.presence()) #add this line for random presence
     print("The Bot Is Now Online And Ready To Work! :)")
-    print(f'OwnerID:{owner_id}')
-    print(f'Extensions Loaded: {len(Iori.extensions)}/{len(startup_extensions)}')
+    print(f'Extensions Loaded: {len(bot.extensions)}/{len(startup_extensions)}')
     print('all module are up!')
     
 bot.run(read_config('token')
